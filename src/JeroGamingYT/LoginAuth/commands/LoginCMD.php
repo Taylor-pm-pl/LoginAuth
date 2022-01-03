e6e3bc977346ba5581845dfa34e2aa7c395a72c5
HieuCoder <mw.sevice@gmail.com>
2022-01-03 16:16:57 +0700

    Update LoginCMD.php

--- src/JeroGamingYT/LoginAuth/commands/LoginCMD.php
+++ src/JeroGamingYT/LoginAuth/commands/LoginCMD.php
@@ -28,6 +28,9 @@ use pocketmine\plugin\PluginOwned;
 
 class LoginCMD extends Command implements PluginOwned
 {	
+	/** @Var Main $plugin */
+	public Main $plugin;
+	
 	/**
 	 * @param Main $plugin
 	 */
